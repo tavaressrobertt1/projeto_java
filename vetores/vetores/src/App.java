933e03d1f75dd9216d4a622e781e80f9b3125b4c
Robert Tavares <tavaressrobertt1@gmail.com>
2026-05-29 21:42:22 -0300

    Usando Scanner

--- vetores/vetores/src/App.java
+++ vetores/vetores/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner (System.in);
+
+        System.out.println("Digite o seu nome");
+        String nome = entrada.nextLine();
+
+        System.out.printf("O nome informado e: %s ", nome);
+
+
+
+        entrada.close();
     }
 }
